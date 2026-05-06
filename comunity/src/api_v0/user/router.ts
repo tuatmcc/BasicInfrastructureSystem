a3c10eb1a8278c0818cd3f79ce82020071fed856
yufoxda <mysubaccoun1@gmail.com>
2026-05-06 12:12:42 +0900

    feat:user api

--- comunity/src/api_v0/user/router.ts
+++ comunity/src/api_v0/user/router.ts
@@ -1,6 +1,15 @@
-import { createRoute, OpenAPIHono, z } from "@hono/zod-openapi";
-import { UserSchema, UpdateUserSchema } from "./schema";
+import { OpenAPIHono} from "@hono/zod-openapi";
 import type { AppContext } from "../../core/types";
+
+import { 
+    createUserRoute,
+    listUsersRoute,
+    getUserMeRoute,
+    getUserByIdRoute,
+    updateUserMeRoute,
+    updateUserByIdRoute,
+    deleteUserByIdRoute
+ } from "./schema";
 import { 
     createUserService, 
     listUsersService,
@@ -20,62 +29,6 @@ import { userRoleRouter } from "./role/router";
 // /role: ユーザーのロール管理ロジック (サブディレクトリへ委譲)
 // *****************
 
-// create
-// ユーザーを新規作成する
-const createUserRoute = createRoute({
-    method: "post",
-    path: "/",
-    responses: { 201: { description: "成功", content: { "application/json": { schema: UserSchema } } } }
-});
-
-// read
-// ユーザー一覧を取得する
-const listUsersRoute = createRoute({
-    method: "get",
-    path: "/",
-    responses: { 200: { description: "成功", content: { "application/json": { schema: UserSchema.array() } } } }
-});
-
-// 自身の情報を取得する
-const getUserMeRoute = createRoute({
-    method: "get",
-    path: "/me",
-    responses: { 200: { description: "成功", content: { "application/json": { schema: UserSchema } } } }
-});
-
-// 特定ユーザーの情報を取得する
-const getUserByIdRoute = createRoute({
-    method: "get",
-    path: "/{id}",
-    request: { params: z.object({ id: z.string() }) },
-    responses: { 200: { description: "成功", content: { "application/json": { schema: UserSchema } } } }
-});
-
-// update
-// 自身の情報を更新する
-const updateUserMeRoute = createRoute({
-    method: "put",
-    path: "/me",
-    request: { body: { content: { "application/json": { schema: UpdateUserSchema } } } },
-    responses: { 200: { description: "成功", content: { "application/json": { schema: UserSchema } } } }
-});
-
-// 特定ユーザーの情報を更新する
-const updateUserByIdRoute = createRoute({
-    method: "put",
-    path: "/{id}",
-    request: { params: z.object({ id: z.string() }), body: { content: { "application/json": { schema: UpdateUserSchema } } } },
-    responses: { 200: { description: "成功", content: { "application/json": { schema: UserSchema } } } }
-});
-
-// delete
-// 特定ユーザーを削除する
-const deleteUserByIdRoute = createRoute({
-    method: "delete",
-    path: "/{id}",
-    request: { params: z.object({ id: z.string() }) },
-    responses: { 204: { description: "成功" } }
-});
 
 export const userRouter = new OpenAPIHono<AppContext>()
     .openapi(createUserRoute, createUserService)
