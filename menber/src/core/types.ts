3cb7ccd218b8960e28aa9ab936817ceca927b1c4
yufoxda <mysubaccoun1@gmail.com>
2026-05-08 21:56:20 +0900

    feat:auth and set appcontext

--- menber/src/core/types.ts
+++ menber/src/core/types.ts
@@ -4,12 +4,13 @@ import type { NodePgDatabase } from 'drizzle-orm/node-postgres'
 
 export type CloudflareBindings = {
   DATABASE_URL: string
+  SUPABASE_URL: string // 例: https://xxxx.supabase.co
 }
 
 export type AppContext = {
   Bindings: CloudflareBindings
   Variables: {
-    db: NodePgDatabase // todo: dbclient
+    db: NodePgDatabase<any>
     appUser: appUser
   }
 }
